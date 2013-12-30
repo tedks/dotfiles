2e840db22619502f4ba5d5ce0d661cd9389348a7
Ted Smith <tedks@riseup.net>
2013-12-29 21:05:43 -0500

    more refactoring

--- geo_access.py
+++ geo_access.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python
 
 import sys, GeoIP
+from itertools import chain
+
+def ips_of_stream(strm):
+    if type(strm) == str:
+        strm = open(strm, "r")
+    ret = {}
+    for l in strm:
+        ip = l.split(' ')[0]
+        if not ip in ret:
+            ret[ip] = []
+        ret[ip].append(l)
+    strm.close()
+    return ret
+
+def list_concat(l1, l2):
+    if l1 is None:
+        l1 = []
+    if l2 is None:
+        l2 = []
+    return l1 + l2
+
+def dict_extend(d1, d2, combine):
+    ret = {}
+    for k in chain(d1.iterkeys(), d2.iterkeys()):
+        v1 = d1[k] if k in d1 else None
+        v2 = d2[k] if k in d2 else None
+        ret[k] = combine(v1, v2)
+    return ret
 
 if __name__ == "__main__":
     gi = GeoIP.open("/usr/share/GeoIP/GeoIP.dat", 0)
-    ips = {}
-    for fn in sys.argv[1:]:
-        f = open(fn, "r")
-        for l in f:
-            ip = l.split(' ')[0]
-            if not ip in ips:
-                ips[ip] = []
-            ips[ip].append(l)
+    ips = reduce(lambda acc, s: dict_extend(acc, ips_of_stream(s), list_concat),
+                 sys.argv[1:], {})
     items = ips.items()
     items.sort(cmp=lambda (k1,v1),(k2,v2): len(v1) - len(v2))
     for k,v in items:
