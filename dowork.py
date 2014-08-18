86e7b3764a16a371086c7f1d63d301df6619eee3
Ted Smith <tedks@riseup.net>
2014-08-17 23:06:52 -0700

    meh

--- dowork.py
+++ dowork.py
@@ -2,9 +2,15 @@
 
 import argparse
 import os
+import subprocess
 
 INITFILE = ".work_init"
 WORKDIR = "workdir"
+
+
+def werk(project, path):
+    os.chdir(path)
+    subprocess.call("byobu")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,4 +22,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     print("args.project: {} track? {}".format(args.project, args.track))
+    if os.path.abspath(args.project) == args.project:
+        print("is an abspath")
+    else:
+        print("is a relative path")
+        print("project: {}".format(os.path.exists("/home/tedks/Projects/{}".format(args.project))))
+
     
