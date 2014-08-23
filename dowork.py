fddb755f1e970608a97acbedba462aa6a122ce6d
Ted Smith <tedks@riseup.net>
2014-08-23 19:56:41 -0400

    little hacking

--- dowork.py
+++ dowork.py
@@ -14,7 +14,22 @@ def werk(project, path):
     assert os.path.exists(project)
     assert os.path.isdir(project)
     os.chdir(path)
+    if WORKDIR in os.listdir():
+        os.chdir(os.path.join(path, WORKDIR))
     subprocess.call("byobu -S {}".format(project))
+    exit(0)
+
+def project(project):
+    if os.path.abspath(project) == project:
+        return os.path.split(project)[-1]
+    else:
+        return project    
+
+def path(project):
+    if os.path.abspath(project) == project:
+        return project
+    else:
+        # pick up here
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,5 +46,16 @@ if __name__ == '__main__':
     else:
         print("is a relative path")
         print("project: {}".format(os.path.exists("/home/tedks/Projects/{}".format(args.project))))
+
+    project = project(args.project)
+    path = path(args.project)
+
+
+
+
+
+
+
+
 
     
