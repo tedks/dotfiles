11483fe2457fac6c1cc1ef4bc77eddee8fa9591c
Ted Smith <tedks@riseup.net>
2017-01-26 23:50:35 -0800

    still doesn't quite work

--- monitor_network.sh
+++ monitor_network.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 while true; do
     ping -c 4 4.2.2.1 > /dev/null 2>&1
-    if [ $? ]; then
-	(echo "`date`: network up!" && sleep 300)
+    if ping -c 4 4.2.2.1 > /dev/null 2>&1; then
+	echo "`date`: network up!" && sleep 300
     else
-	killall nm-applet;
+	killall nm-applet
+	disown nm-applet >/dev/null 2>&1 
     fi;
 done
